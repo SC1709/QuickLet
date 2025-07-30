1ab6446d6240a32881ac30c7d749e27c3a42219f
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-07-30 22:14:58 +0530

    added new arrival and product selected one

---
+++ frontend/src/components/Details/BestSeller.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductDetails from '../Products/ProductDetails'
+
+const BestSeller = () => {
+  return (
+    <div>
+        <h2 className='text-3xl font-bold text-center mb-4'>Best Seller</h2>
+        <ProductDetails />
+    </div>
+  )
+}
+
+export default BestSeller
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
 import NewArrival from '../components/Products/NewArrival';
+import BestSeller from '../components/Details/BestSeller';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <GenderCollectionSection />
         <NewArrival />
+        <BestSeller />
     </div>
   )
 }

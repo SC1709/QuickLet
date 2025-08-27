98cf58600a324e81dba9f6223cdfc772c1889104
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-08-27 12:46:45 +0530

    added backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv=require("dotenv")
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
 });

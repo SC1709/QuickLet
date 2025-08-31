7f28a1802dfba6f03b93e380b71f11a5611542db
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-08-31 21:15:41 +0530

    added checkout route in server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dbConnect = require("./config/db");
 const userRoutes = require("./routes/UserRoutes");
 const ProductRoutes = require("./routes/ProductRoutes");
 const CartRoutes = require("./routes/CartRoutes");
+const CheckoutRoutes = require("./routes/CheckoutRoute");
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/users", userRoutes);
 app.use("/api/products", ProductRoutes);
 app.use("/api/cart", CartRoutes);
+app.use("/api/checkout", CheckoutRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);

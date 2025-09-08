463e26d2da62e4a9aaafb86b4e05279dd3b6fbf7
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-09-08 22:24:09 +0530

    added cart slice reducer in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import productSlice from "./slices/productSlice";
+import productReducer from "./slices/productSlice";
+import cartReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    products: productSlice,
+    products: productReducer,
+    cart: cartReducer,
   },
 });
 

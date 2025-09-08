9191ea1162ea933faa35e547304118239fe90f3d
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-09-08 22:41:18 +0530

    added checkout slice and reducer

--- frontend/src/redux/slices/checkoutSlice.js
+++ frontend/src/redux/slices/checkoutSlice.js
@@ -22,3 +22,30 @@ export const createCheckoutSession = createAsyncThunk(
     }
   }
 );
+
+const checkoutSlice = createSlice({
+  name: "checkout",
+  initialState: {
+    checkout: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCheckoutSession.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createCheckoutSession.fulfilled, (state, action) => {
+        state.loading = false;
+        state.checkout = action.payload;
+      })
+      .addCase(createCheckoutSession.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+
+export default checkoutSlice.reducer;

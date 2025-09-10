82309e2d89e2a50b642eb394448f5c81d3d2548a
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-09-10 20:47:29 +0530

    added delete and update product thunk

--- frontend/src/redux/slices/admin/adminProductSlice.js
+++ frontend/src/redux/slices/admin/adminProductSlice.js
@@ -37,3 +37,39 @@ export const addProduct = createAsyncThunk(
   }
 );
 
+// async thunk to update admin products
+export const updateProduct = createAsyncThunk(
+  "admin/updateProduct",
+  async ({ id, productData }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(
+        `${backendUrl}/api/admin/products/${id}`,
+        productData,
+        {
+          headers: {
+            Authorization: USER_TOKEN,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// async thunk to delete admin products
+export const deleteProduct = createAsyncThunk(
+  "admin/deleteProduct",
+  async (id) => {
+    const response = await axios.delete(
+      `${backendUrl}/api/admin/products/${id}`,
+      {
+        headers: {
+          Authorization: USER_TOKEN,
+        },
+      }
+    );
+    return response.data;
+  }
+);

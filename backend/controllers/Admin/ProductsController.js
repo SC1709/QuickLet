dc07f4bfc9d7434ba7929c23ce30e1e8f5f3a68e
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-09-23 22:14:38 +0530

    changed product admin as per frontend need

--- backend/controllers/Admin/ProductsController.js
+++ backend/controllers/Admin/ProductsController.js
@@ -3,7 +3,7 @@ const Product = require("../../models/Product");
 const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json({ message: "Products found successfully", products });
+    res.status(200).json(products, { message: "Products found successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
